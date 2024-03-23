ab6d89f8eb1d74000f920b5e9fe97ccfe3c9688e
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-03-23 17:12:39 +0000

    update env-variable names

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabaseUrl = process.env.SUPABASE_URL;
 
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
