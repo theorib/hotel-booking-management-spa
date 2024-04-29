24825085ce1e2289659aa6c8319ba5028a74b4d5
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-04-29 21:47:59 +0100

    change package manager to pnpm and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import { Suspense, lazy } from 'react';
 
